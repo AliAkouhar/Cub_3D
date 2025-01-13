6c68d1475d7997cb6682fe89cb98df3ec89ac277
fbazaz <fbazaz@c3r8p1.1337.ma>
2025-01-13 13:28:00 +0100

    separate mandatory and bonus parts

---
+++ bonus/utils2_bonus.c
@@ -0,0 +1,26 @@
+#include "../headers/cub3d.h"
+
+float	radian_to_degree(float corner)
+{
+	return (corner * (180 / PI));
+}
+
+int	get_right_texture(t_cub *cub, t_point endPoint)
+{
+	if (endPoint.ver_inter)
+	{
+		if (cub->player.current_ray_angle > 3 * PI / 2
+			|| cub->player.current_ray_angle < PI / 2)
+			return (EAST);
+		else
+			return (WEST);
+	}
+	else
+	{
+		if (cub->player.current_ray_angle > 0
+			&& cub->player.current_ray_angle < PI)
+			return (SOUTH);
+		else
+			return (NORTH);
+	}
+}
